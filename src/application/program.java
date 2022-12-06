7fa5e287b445a752c30220f1f6d1597f62f2619f
lucaspp19 <lucaspp191@gmail.com>
2022-12-06 11:50:51 -0300

    Department entity class

---
+++ src/application/program.java
@@ -0,0 +1,15 @@
+package application;
+
+import model.entities.departments;
+
+public class program {
+
+	public static void main(String[] args) {
+		
+		departments dp = new departments(1, "book");
+		
+		System.out.println(dp);
+
+	}
+
+}
